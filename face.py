6fedc5f78bb1b6666c0de949d8cd15271d77fdca
RiadAhmed-prog <53094420+RiadAhmed-prog@users.noreply.github.com>
2019-07-29 21:10:09 +0600

    Update face.py

--- face.py
+++ face.py
@@ -12,17 +12,17 @@ from playsound import playsound
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("riad.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+riad_image = face_recognition.load_image_file("riad.jpg")
+riad_face_encoding = face_recognition.face_encodings(riad_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("inan.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+inan_image = face_recognition.load_image_file("inan.jpg")
+inan_face_encoding = face_recognition.face_encodings(inan_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
+    riad_face_encoding,
+    inan_face_encoding
 ]
 known_face_names = [
     "RIAD",
